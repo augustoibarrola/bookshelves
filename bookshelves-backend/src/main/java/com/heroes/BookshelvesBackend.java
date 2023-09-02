8535029ad074726785736eca7c4597fb8efcc31f
augustoibarrola <augusto.t.ibarrola@gmail.com>
2023-09-02 14:11:48 -0400

    the great renaming begins

--- bookshelves-backend/src/main/java/com/heroes/BookshelvesBackend.java
+++ bookshelves-backend/src/main/java/com/heroes/BookshelvesBackend.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HeroesApplication {
+public class BookshelvesBackend {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HeroesApplication.class, args);
+		SpringApplication.run(BookshelvesBackend.class, args);
 	}
 
 }
